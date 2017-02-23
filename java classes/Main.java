c7609f5de3fe704098d4083085e58f335e9c1d95
David Heras <davidherasp@gmail.com>
2017-02-23 23:10:12 +0100

    Added comment in Main

--- java classes/Main.java
+++ java classes/Main.java
@@ -5,6 +5,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException 
     {
+    	// Set your API key here if you want the recent soloQ data from the players. You can also set the api key as a run parameter
         RiotAPI.setAPIKey(args[0]);
         DAO dao = new DAO();
         
